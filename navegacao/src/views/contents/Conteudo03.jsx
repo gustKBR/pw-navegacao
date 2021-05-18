6aa0c0c47190727378b0a0feab4b96abc53845f1
gustKBR <gustavocabralcorreia@gmail.com>
2021-05-18 19:24:41 +0000

    React 03 - Navegacao
    Signed-off-by: gustKBR <gustavocabralcorreia@gmail.com>

---
+++ navegacao/src/views/contents/Conteudo03.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Conteudo03 = props => (
+    <div>
+        <h1>Conteúdo 03</h1>
+    </div>
+)
+export default Conteudo03
---
+++ navegacao/src/views/contents/NaoEncontrado.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const NaoEncontrado = props => (
+    <div>
+        <h1>Página não encontrada.</h1>
+    </div>
+)
+export default NaoEncontrado
